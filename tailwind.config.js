36673a772c31cc7ac51998cb74742f47e0872e44
Nikita Singh <nikitasingh.1022.com>
2023-03-30 14:42:58 +0530

    mediaQueryNotWorking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["*"],
   theme: {
@@ -6,7 +8,20 @@ module.exports = {
     //   Macondo : ['Macondo', 'cursive']
     // },
     screens:{
-      'small':'0px'
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
     },
     extend: {
       backgroundImage: {
